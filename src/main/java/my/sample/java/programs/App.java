107a0ef58f3f9324f88cee11bdc4848868010bfd
Venkatesh <venkateshreddy.u@gmail.com>
2018-07-10 22:13:42 +0530

    initial

---
+++ src/main/java/my/sample/java/programs/App.java
@@ -0,0 +1,13 @@
+package my.sample.java.programs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
